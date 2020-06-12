66e892e05d9d188b4cfe40645b30aae23d8c6801
NafiuNoman <nafiunoman@gmail.com>
2020-06-12 22:50:53 +0600

    CorrectedValue

--- app/src/main/java/com/example/practicevolley/Connect.java
+++ app/src/main/java/com/example/practicevolley/Connect.java
@@ -48,13 +48,49 @@ public void Start()
                 MainActivity.status.setText(arrayObj.getString("main"));
 
 
+                /*its working but its cant show int value its shows doubble value case getString();fuction take value as a string so its doubble came within it
+                and its show the doubble value if exists
+
                 MainActivity.temp.setText(obj.getString("temp"));
-               /* String a= obj.getString("temp");
+
+                 */
+
+
+
+
+
+                //not working getInt
+               // MainActivity.temp.setText(obj.getInt("temp"));
+
+
+               /*
+                not working in this way
+
+                String a= obj.getString("temp");
                 Integer b = Integer.parseInt(a);
                 MainActivity.temp.setText(b);*/
 
-                MainActivity.maxTemp.setText(obj.getString("temp_max"));
-                MainActivity.minTemp.setText(obj.getString("temp_min"));
+/*
+                  working Hahahaha atlast
+                  this way you have get the int value from JSONobject as a int but you cant directly set int value in textView
+                  so you have to use String.valueOf(); funtion its takes any data type and make it as a String . Now you can
+                  int your int value as String in the field.
+
+
+                  int tm =obj.getInt("temp_max");
+                  int tmi =obj.getInt("temp_min");
+
+                  String Stm=String.valueOf(tm);
+                  String stmi=String.valueOf(tmi);
+
+                  MainActivity.minTemp.setText(stmi);
+                  MainActivity.maxTemp.setText(Stm);
+
+
+ */             MainActivity.temp.setText(String.valueOf(obj.getInt("temp")));
+                MainActivity.minTemp.setText(String.valueOf(obj.getInt("temp_min")));
+                MainActivity.maxTemp.setText(String.valueOf(obj.getInt("temp_max")));
+
 
                 MainActivity.country.setText(obj2.getString("country"));
 
